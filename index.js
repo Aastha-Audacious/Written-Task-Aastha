3fe4b1eb717e84271f253915dba8d6ab1d31f914
Aastha0710 <aastha.panwar@techiegent.com>
2023-03-06 16:36:23 +0530

    feature: first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+let arr = [
+    {
+      id: 1,
+      name: "Aman",
+    },
+    {
+      id: 2,
+      name: "Priya",
+    },
+    {
+      id: 3,
+      name: "Urvashi",
+    },
+    {
+      id: 4,
+      name: "Jay",
+    },
+    {
+      id: 5,
+      name: "Aman",
+    },
+    {
+      id: 6,
+      name: "Roshni",
+    },
+    {
+      id: 7,
+      name: "Nikita",
+    },
+    {
+      id: 7,
+      name: "Shiv",
+    },
+    {
+      id: 8,
+      name: "Teena",
+    },
+    {
+      id: 9,
+      name: "Roshni",
+    },
+    {
+      id: 10,
+      name: "Preeti",
+    },
+  ];
+   
+  
+  
